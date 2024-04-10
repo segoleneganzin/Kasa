4895364673823f0353158740a5f50505e7755ff1
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-04-10 08:53:25 +0200

    router update

--- src/pages/HousingSheet.jsx
+++ src/pages/HousingSheet.jsx
@@ -1,7 +1,7 @@
 import { useFetch } from '../api/useFetch';
 import { useParams } from 'react-router-dom';
 const HousingSheet = () => {
-  const { id: housingId } = useParams();
+  const { housingId } = useParams();
   let filteredHousing;
   const { data, isLoading, error } = useFetch(
     window.location.origin + `/src/assets/housingsAds.json`
