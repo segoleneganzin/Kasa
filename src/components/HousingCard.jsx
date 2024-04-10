3d393ddb964aa228e94d41002132fe3041297418
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-04-10 10:07:44 +0200

    responsive home

--- src/components/HousingCard.jsx
+++ src/components/HousingCard.jsx
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 const HousingCard = ({ housingId, housings }) => {
   const filterHousing = housings.filter((housing) => housing.id == housingId);
   const housing = filterHousing[0];
-  // console.log(filterHousing);
   const housingPicture = housing.cover;
 
   return (
     <Link to={`/logement/${housingId}`}>
       <article className='housing-card'>
-        <h2 className='housing-card__title'>{housing.title}</h2>
         <img src={housingPicture} alt='' className='housing-card__picture' />
+        <h2 className='housing-card__title'>{housing.title}</h2>
       </article>
     </Link>
   );
