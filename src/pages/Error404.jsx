2cb063018a3d0bb295f4dbf98c0e81140d444205
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-04-12 10:59:53 +0200

    error 404 page set up

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Error404 = () => {
   return (
-    <main>
-      <h1>Erreur 404</h1>
+    <main className='error-page'>
+      <h1 className='error-page__type'>404</h1>
+      <p className='error-page__message'>
+        Oups! La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to={'/'} className='error-page__link'>
+        Retourner sur la page d&apos;accueil
+      </Link>
     </main>
   );
 };
